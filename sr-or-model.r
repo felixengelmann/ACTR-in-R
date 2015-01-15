619b8b642800561e4745f12ad8b16193f08722a4
Felix Engelmann <xilef@posteo.net>
2015-01-15 14:38:25 +0100

    edit rc model

--- sr-or-model.r
+++ sr-or-model.r
@@ -20,7 +20,7 @@
 ###
 #########################################################################################################
 
-silent.mode <<- FALSE
+silent.mode <<- TRUE
 use.standard.error <<- TRUE
 
 ## Experiment definitions
@@ -102,7 +102,7 @@ num.experiments <- length(experiments);
 
 ## number of monte carlo trials per experiment
 
-default.trials <- 1000;
+default.trials <- 100;
 simulated.experiments <- 50;
 
 
@@ -110,9 +110,8 @@ simulated.experiments <- 50;
 ## of possible values rather than a single value.  
 
 ## Latency factor
-F <- c(0.14)
-# F <- c(0.5, 0.6);
-# F <- c(0.14, 0.4, 0.5, 0.6);
+# F <- c(0.14)
+F <- c(0.14, 0.2, 0.5, 0.7);
 
 ## Extra category penalty
 cat.penalty <- c(-999);
@@ -121,6 +120,7 @@ cat.penalty <- c(-999);
 
 ## Total source activation
 G <- c(1.0);
+# G <- c(0.75, 1.0, 1.25);
 
 ## Activation noise parameter for logistic distribution
 ans  <- c(0.15)
@@ -172,22 +172,23 @@ save(results, all.runs, param.results, combo.summary, num.parameters, num.combin
 # plot.best.overall.decay.no.mp();
 # plot.individual.decay.no.mp();
 
-plot.best.overall.decay();
-plot.individual.decay();
-
+# plot.individual.decay();
 # plot.full.range.no.decay();
-plot.full.range.decay();
+# plot.full.range.decay();
 
+plot.best.overall.decay();
 
 ### Results
 results
 all.runs
 param.results
 combo.summary
+subset(combo.summary, mse==min(mse))
 
 
 ## Plot all combos:
 plot.all(prefix="plots/")
+
 
 ## Plot combos with G==1:
 p <- combo.summary$combo[combo.summary$G==1]
@@ -198,10 +199,10 @@ predictions <- all.runs[,c(13,23:27)]
 all.latencies <- lapply(predictions$all.crit.latencies, function(x) as.numeric(unlist(strsplit(toString(x),","))))
 write.table(predictions, "predictions.txt")
 
-## Export combo 10:
+## Export combo 1:
 head(all.runs)
 exp <- experiments[[1]]$name
-combo <- 10
+combo <- 1
 index <- seq(from=combo, to=total.runs, by=num.combinations);
 predictions <- all.runs[index,c(13,23:26)]
 
